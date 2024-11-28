0d4db38a538be24a5a222def2b8815b7ea70b71e
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2024-11-28 12:11:54 +0700

    feat: login register page, RPButton, RPTextFormField, RPDropdownButton, RPBottomNavbar

---
+++ lib/screens/minuman/route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MinumanRoute extends StatelessWidget {
+  const MinumanRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Daftar Minuman'),
+    );
+  }
+}
---
+++ lib/utils/color_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RPColors {
+  static const Color merahTua = Color(0xFFD04848);
+  static const Color merahMuda = Color(0xFFFF5757);
+  static const Color biruTua = Color(0xFF3B747A);
+  static const Color biruMuda = Color(0xFF4F9DA6);
+  static const Color kuningMuda = Color(0xFFFACF5A);
+  static const Color kuningTua = Color(0xFFC2A046);
+  static const Color textFieldBackground = Color(0xFFF3F4F6);
+  static const Color textFieldPlaceholder = Color(0xFF737373);
+}
