d7217760a4d59c92fb3599e857547912acaae594
AthallahD <athallah2209@gmail.com>
2024-12-09 15:40:07 +0700

    feat: edit route for makanan

--- lib/screens/makanan/route.dart
+++ lib/screens/makanan/route.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:rasapalembang/screens/makanan/makanan_list.dart';
 
 class MakananRoute extends StatelessWidget {
   const MakananRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Daftar Makanan'),
+    return Scaffold(
+      body: MakananListPage(),
     );
   }
 }
