e969f3bb7a2ed224f7a780d23f075a6f2f73c621
ANDI AQSA MAPPATUNRU MARZUKI <andiaqsa365@gmail.com>
2024-12-09 14:54:38 +0700

    feat: favorit

---
+++ lib/screens/favorit/route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoritRoute extends StatelessWidget {
+  const FavoritRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Daftar Favorit'),
+    );
+  }
+}
