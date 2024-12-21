42d348ff0b0ed7c3b67556bcc739a8611148bf74
AthallahD <athallah2209@gmail.com>
2024-12-21 15:28:43 +0700

    slight change

--- lib/widget/rp_makanan_detail.dart
+++ lib/widget/rp_makanan_detail.dart
@@ -153,8 +153,4 @@ class _RPMakananDetailState extends State<RPMakananDetail> {
       ),
     );
   }
-
-  String _title(String string) {
-    return "${string[0].toUpperCase()}${string.substring(1).toLowerCase()}";
-  }
 }
