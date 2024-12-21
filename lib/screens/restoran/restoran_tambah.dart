778ddc645d99cebc55cded076a36b9b1eaa4acdc
Muhammad Fawwaz EFS <faw.edsa@gmail.com>
2024-12-22 00:04:59 +0700

    feat:sambungin backend edit tambah, nyamain imagepicker, benerin direktori file file

---
+++ lib/screens/restoran/restoran_tambah.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:rasapalembang/widget/restoran_form.dart';
+
+class RestoranTambahPage extends StatelessWidget {
+  const RestoranTambahPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RestoranFormPage(
+      imagePickerLabel: 'Tambah Gambar Restoran',
+      saveButtonLabel: 'Simpan Restoran',
+    );
+  }
+}
