da11fdeff7eef4a7a25511a8a5cfe722f61773dd
Fazil <faziltirtana@tutanota.com>
2024-12-22 17:24:04 +0700

    feat: update base URL for the application to the production server

--- lib/utils/urls_constants.dart
+++ lib/utils/urls_constants.dart
@@ -1,4 +1,6 @@
 class RPUrls {
-  static const String baseUrl = 'http://127.0.0.1:8000';
-  static const String noProfileUrl = 'https://raw.githubusercontent.com/D02-PBP-2024/RasaPalembang/refs/heads/master/static/images/avatar.png';
+  static const String baseUrl =
+      'http://muhammad-fazil31-rasapalembang.pbp.cs.ui.ac.id';
+  static const String noProfileUrl =
+      'https://raw.githubusercontent.com/D02-PBP-2024/RasaPalembang/refs/heads/master/static/images/avatar.png';
 }
